5ab8087ca2e7bc643ba4a31224107d564ec2377b
Karthik Grsk <karthikgrsk@gmail.com>
2025-05-15 00:33:06 +0530

    deleted student

--- src/main/java/com/example/ThinkDesk/StudentController.java
+++ src/main/java/com/example/ThinkDesk/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,6 +44,12 @@ public class StudentController {
         student.setCgpa(9.8);
         repo.save(student);
         return student;
+    }
+
+    @DeleteMapping("/student/delete/{id}")
+    public void deleteStudent(@PathVariable int id){
+        Student student=repo.findById(id).get();
+        repo.delete(student);
     }
     
 }
